54977fa5db2615de6bb482a7c5d3226be25673e4
martinszkita <marpozniak1@gmail.com>
2025-07-14 17:54:06 +0200

    underline random word in rate_word

--- gamification/quiz/urls.py
+++ gamification/quiz/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.quiz, name='quiz' ),
     path('sentence/', views.display_sentence, name='display_sentence'),
+    path('rate_word', views.rate_word, name="rate_word"),
     
     ]
--- gamification/quiz/views.py
+++ gamification/quiz/views.py
@@ -1,30 +1,47 @@
 from django.shortcuts import render, redirect
 from .models import Book
 
-
 # Create your views here.
 def quiz(request):
-    return redirect(display_sentence)
+    return render(request, 'quiz/quiz.html')
 
+def get_random_sentence(tekst):
+    import random
+    import re
+
+    zdania = re.split(r'(?<=[.!?])\s+', tekst.strip())
+    zdania = [z.strip() for z in zdania if z.strip()] 
+
+    if not zdania:
+        return "Brak zdań w tekście."
+
+    return random.choice(zdania)
+
+    
 def display_sentence(request):
 
 
-    def get_random_sentence(tekst):
-        import random
-        import re
-        # Dzieli tekst na zdania na podstawie kropki, znaku zapytania i wykrzyknika
-        zdania = re.split(r'(?<=[.!?])\s+', tekst.strip())
-        zdania = [z.strip() for z in zdania if z.strip()]  # Usuwa puste
-
-        if not zdania:
-            return "Brak zdań w tekście."
-
-        return random.choice(zdania)
-        
     book = Book.objects.get(title="Berenice")
 
     random_sentence = get_random_sentence(book.text)
     
-
     return render(request, 'quiz/sentence.html', {
         'random_sentence': random_sentence})
+    
+# gra nr1 - ocena dopasowania slowa w zdaniu
+def rate_word(request):
+    def mark_word(word):
+        return (f"<u>{word}</u>")
+        
+    import random
+    book = Book.objects.get(title="Berenice")
+    random_sentence = get_random_sentence(book.text)
+    random_word = random.choice(random_sentence.split())
+
+
+    random_sentence=random_sentence.replace(random_word, mark_word(random_word), 1)
+
+    
+    context = {'random_sentence': random_sentence, 'random_word': random_word}
+    
+    return render(request, 'quiz/rate_word.html', context)
